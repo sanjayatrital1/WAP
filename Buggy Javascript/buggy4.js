31b8cdd6807e6dfc710b8e193ec452eb4190933a
Sanjaya Trital <snjtrital@gmail.com>
2019-05-31 18:28:41 -0500

    update buggy javascript

---
+++ Buggy Javascript/buggy4.js
@@ -0,0 +1,16 @@
+let somethingElse= function () {
+    'use strict';
+    let greater=5 > 2;
+        if (greater) {
+            alert("Bigger");
+            }
+        let smaller= 5 < 2;
+        if (smaller) {
+            alert("Smaller");
+        }
+    };
+    
+    let gogogo = function () {
+    'use strict';
+        alert("Yay, it works!");
+    };
