8d26c109dde2888cfda6cf44c182ceeaa760f216
Sanjaya Trital <33197944+sanjayatrital1@users.noreply.github.com>
2019-06-04 12:05:59 -0500

    update changes

--- decorateCode/decoratemytext.js
+++ decorateCode/decoratemytext.js
@@ -1,10 +1,18 @@
 window.onload = function(){
-    `use strict`;
+    'use strict';
+    let value =false;
+    let timerId;
     let decorateButton = document.getElementById("decoration");
     decorateButton.onclick= function(){
-        let timerId =setInterval(increaseSize, 500);
+        if(value){
+            value = false;
+            stop(timerId);
+        }
+        else{
+            value = true;
+            timerId =setInterval(increaseSize, 500);
+        }
     };
-
 
    let blingBox = document.getElementById("bling");
    blingBox.onchange = function(){
@@ -14,15 +22,25 @@ window.onload = function(){
         textArea.style.fontWeight="bold";
         textArea.style.color="green";
         textArea.style.textDecoration="underline";
+        document.body.style.backgroundImage= "URL('http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
+        document.body.style.background.color="transparent";
     }
     else{
         document.getElementById("simpletext").style.fontWeight="normal";
+        document.getElementById("simpletext").style.textDecoration="none";
+        document.body.style.backgroundImage = "none";
     }
    }; 
 };
 
 let increaseSize= function(){
+'use strict';
     let textElement= document.getElementById("simpletext");
         let cSSprop = window.getComputedStyle(textElement,null).getPropertyValue("font-size");
         textElement.style.fontSize = parseInt(cSSprop)+ 2 +"px";
-}
+};
+
+let stop = function(id){
+'use strict';
+    clearInterval(id);
+};
