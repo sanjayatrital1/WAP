2e7add2361006794cabe49091399775da495532e
Sanjaya Trital <snjtrital@gmail.com>
2019-06-06 18:41:11 -0500

    update maze game

---
+++ Maze/maze.js
@@ -0,0 +1,38 @@
+(function(){
+    'use strict';
+
+    let result;
+    let initial;
+    let msg;
+    
+    $(function(){
+        alert("Click box S to start");
+        $("#start").click(start);
+    });
+    let ready = function(){
+        result = true;
+        initial = true;
+       $("div.boundary").removeClass("youlose");
+    };
+    const halt = function(){
+        result = false;
+        terminate();
+    };
+    const start = function(){
+        ready();
+        $(".boundary").mouseenter(halt);
+        $("#maze").mouseleave(halt);
+        $("#end").mouseenter(terminate);
+    };
+   const terminate =  function(){
+        if(initial){
+            let msg = result ? "You win! :]" : "Sorry, you lost :[";
+            //if(!result) alert(msg);
+           if(!result) $("div.boundary").addClass("youlose");
+           //alert(msg);
+           $("#status").text(msg);
+            initial = false;
+           
+        }
+    };
+})();
